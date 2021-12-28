8a05abef1131a4608bd31bfcfc2fb14c7788aaa8
Lexie <6937574+Lexiebean@users.noreply.github.com>
2021-12-28 14:14:01 +1100

    Add files via upload

---
+++ TurtleMenagerie.lua
@@ -0,0 +1,38 @@
+local pets = {}
+local mounts = {}
+
+local function FindFriends()
+	for i = 1, MAX_SKILLLINE_TABS do
+	   local name, texture, offset, numSpells = GetSpellTabInfo(i);
+	   
+	   if not name then
+		  break;
+	   end
+
+		if name == "ZCompanions" then
+			for s = offset + 1, offset + numSpells do
+				local spell, rank = GetSpellName(s, BOOKTYPE_SPELL);
+				table.insert(pets, spell)
+			end
+		end
+		
+		if name == "ZMounts" then
+			for s = offset + 1, offset + numSpells do
+				local spell, rank = GetSpellName(s, BOOKTYPE_SPELL);
+				table.insert(mounts, spell)
+			end
+		end
+	end
+end
+
+SLASH_MENAGERIEPET1 = "/randompet"
+SlashCmdList["MENAGERIEPET"] = function()
+	FindFriends()
+	CastSpellByName(pets[math.random(table.getn(pets))])
+end
+
+SLASH_MENAGERIEMOUNT1 = "/randommount"
+SlashCmdList["MENAGERIEMOUNT"] = function()
+	FindFriends()
+	CastSpellByName(mounts[math.random(table.getn(mounts))])
+end
