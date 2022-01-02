b17237d69cd5791cd426435bdcd6e660abe4d1c4
Lexie <6937574+Lexiebean@users.noreply.github.com>
2022-01-03 04:08:26 +1100

    Turtle Menagerie v2
    - `/randmount` should no longer attempt to summon bug mounts outside of AQ40. (experimental - let me know if it worked please)
    - And only attempt to summon bug mounts and no others within AQ40. (experimental - let me know if it worked please)
    - Added a blacklist function which can be used to prevent unwanted collected pets and mounts from being randomly summoned.
    
    `/randmount ls` or `/randpet ls` - Lists all blacklisted pets and mounts
    
    `/randpet bl _pet_` - Blacklists a pet. eg. /randpet bl Lulu - Pets can be linked from the collection tab for this command.
    `/randmount bl _mount_` - Blacklists a mount. eg. /randpet bl [Riding Turtle] - Mounts can be linked from the collection tab for this command.
    
    `/randpet rm _number_` - Removes the nᵗʰ entry from the blacklist. Check `/randpet ls` eg. /randpet rm 1
    `/randmount rm _number_` - Removes the nᵗʰ entry from the blacklist. Check `/randmount ls` eg. /randmount rm 1

--- TurtleMenagerie.lua
+++ TurtleMenagerie.lua
@@ -1,6 +1,8 @@
-local pets = {}
-local mounts = {}
+if not Menagerie_Pets then Menagerie_Pets = { } end
+if not Menagerie_Mounts then Menagerie_Mounts = { } end
+local gfind = string.gmatch or string.gfind
 
+-- Generate the list of friends
 local function FindFriends()
 	for i = 1, MAX_SKILLLINE_TABS do
 	   local name, texture, offset, numSpells = GetSpellTabInfo(i);
@@ -12,27 +14,118 @@ local function FindFriends()
 		if name == "ZCompanions" then
 			for s = offset + 1, offset + numSpells do
 				local spell, rank = GetSpellName(s, BOOKTYPE_SPELL);
-				table.insert(pets, spell)
+				
+				-- Check for blacklisted pets
+				local bl = false
+				for i=1,table.getn(Menagerie_Pets) do
+					if  string.lower(spell) == Menagerie_Pets[i] then
+						bl = true
+					end
+				end
+				if not bl then 
+					table.insert(pets, spell)
+				end
 			end
 		end
 		
 		if name == "ZMounts" then
 			for s = offset + 1, offset + numSpells do
 				local spell, rank = GetSpellName(s, BOOKTYPE_SPELL);
-				table.insert(mounts, spell)
+
+				-- Check for blacklisted mounts
+				local bl = false
+				for i=1,table.getn(Menagerie_Mounts) do
+					if  string.lower(spell) == Menagerie_Mounts[i] then
+						bl = true
+					end
+				end
+				if not bl then
+					-- Dirty check to see if we're in AQ40 or not and only add the appropriate mounts to the list. ...I'm tired, this is messy. But probably works? IDK
+					if GetZoneText() ~= "Temple of Ahn'Qiraj" then
+						if spell ~= "Red Qiraji Resonating Crystal" or spell ~= "Green Qiraji Resonating Crystal" or spell ~= "Blue Qiraji Resonating Crystal" then
+							table.insert(mounts, spell)
+						end
+					else
+						if spell == "Red Qiraji Resonating Crystal" or spell == "Green Qiraji Resonating Crystal" or spell == "Blue Qiraji Resonating Crystal" or spell == "Black Qiraji Resonating Crystal" then
+							table.insert(mounts, spell)
+						end
+					end
+				end
 			end
 		end
 	end
 end
 
 SLASH_MENAGERIEPET1, SLASH_MENAGERIEPET2 = "/randompet", "/randpet"
-SlashCmdList["MENAGERIEPET"] = function()
-	FindFriends()
-	CastSpellByName(pets[math.random(table.getn(pets))])
+SlashCmdList["MENAGERIEPET"] = function(message)
+	Menagerie("pets " .. message)
 end
 
 SLASH_MENAGERIEMOUNT1, SLASH_MENAGERIEMOUNT2 = "/randommount", "/randmount"
-SlashCmdList["MENAGERIEMOUNT"] = function()
+SlashCmdList["MENAGERIEMOUNT"] = function(message)
+	Menagerie("mounts " .. message)
+end
+
+-- A lot of this blacklist code is heavily based on Shagu's ShaguChat.
+function Menagerie(message)
+
+	pets = {}
+	mounts = {}
+
+	local commandlist = { }
+	local command
+
+	for command in gfind(message, "[^ ]+") do
+		table.insert(commandlist, string.lower(command))
+	end
+	
 	FindFriends()
-	CastSpellByName(mounts[math.random(table.getn(mounts))])
+	
+	-- Add to blacklist
+	if commandlist[2] == "bl" then
+		local addstring = table.concat(commandlist," ",3)
+		if addstring == "" then return end
+		local m = strmatch(addstring, "%[(.+)%]")
+		if m then addstring = m end
+		if commandlist[1] == "pets" then
+			table.insert(Menagerie_Pets, string.lower(addstring))
+		else
+			table.insert(Menagerie_Mounts, string.lower(addstring))
+		end
+		DEFAULT_CHAT_FRAME:AddMessage("|cffbe5eff[Turtle Menagerie]|r Adding |cffbe5eff".. addstring .."|r to your blacklist list.")
+
+	-- Remove from blacklist
+	elseif commandlist[2] == "rm" then
+		if Menagerie_Pets[tonumber(commandlist[3])] ~= nil then
+			DEFAULT_CHAT_FRAME:AddMessage("|cffbe5eff[Turtle Menagerie]|r Removing |cffbe5eff" .. Menagerie_Pets[tonumber(commandlist[3])]
+			.. "|r from your blacklist list")
+			table.remove(Menagerie_Pets, commandlist[3])
+		elseif Menagerie_Mounts[tonumber(commandlist[3] - table.getn(Menagerie_Mounts))] ~= nil then
+			DEFAULT_CHAT_FRAME:AddMessage("|cffbe5eff[Turtle Menagerie]|r Removing |cffbe5eff" .. Menagerie_Mounts[tonumber(commandlist[3] - table.getn(Menagerie_Mounts))]
+			.. "|r from your blacklist list")
+			table.remove(Menagerie_Mounts, commandlist[3] - table.getn(Menagerie_Mounts))
+		end
+	
+	-- List the blacklist
+	elseif commandlist[2] == "ls" then
+		DEFAULT_CHAT_FRAME:AddMessage("|cffbe5eff[Turtle Menagerie] Blacklist|r ")
+		DEFAULT_CHAT_FRAME:AddMessage("|cffbe5effPets:")
+		printID = 0
+		for id, bl in pairs(Menagerie_Pets) do
+			DEFAULT_CHAT_FRAME:AddMessage(" |r[|cffbe5eff"..id.."|r] "..bl)
+			printID = id
+		end
+		DEFAULT_CHAT_FRAME:AddMessage("|cffbe5effMounts:")
+		for id, bl in pairs(Menagerie_Mounts) do
+			DEFAULT_CHAT_FRAME:AddMessage(" |r[|cffbe5eff"..id+printID.."|r] "..bl)
+		end
+		
+	-- Summon our friends!
+	else
+		if commandlist[1] == "pets" then
+			CastSpellByName(pets[math.random(table.getn(pets))])
+		else
+			CastSpellByName(mounts[math.random(table.getn(mounts))])
+		end
+	end
 end
